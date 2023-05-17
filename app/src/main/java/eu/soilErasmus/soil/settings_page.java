01b3d627f25d22a6eecc0e57a2e821bf6f82304e
SoftMindsKoinsep <124435144+SoftMindsKoinsep@users.noreply.github.com>
2023-05-17 20:54:40 +0300

    Soil Beta

--- app/src/main/java/eu/soilErasmus/soil/settings_page.java
+++ app/src/main/java/eu/soilErasmus/soil/settings_page.java
@@ -3,6 +3,7 @@ package eu.soilErasmus.soil;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
@@ -17,7 +18,7 @@ import android.widget.Toast;
 public class settings_page extends AppCompatActivity {
 
     DatabaseHelper databaseHelper;
-    RelativeLayout accountData,logOut;
+    RelativeLayout accountData,logOut,deleteAccount;
     Button backButton;
     SharedPreferences pref;
     SharedPreferences.Editor editor;
@@ -31,6 +32,7 @@ public class settings_page extends AppCompatActivity {
 
         backButton = findViewById(R.id.backButton);
         accountData = findViewById(R.id.accountButton);
+        deleteAccount = findViewById(R.id.delete_button);
 
         pref = getSharedPreferences("Data",MODE_PRIVATE);
         editor = pref.edit();
@@ -56,6 +58,13 @@ public class settings_page extends AppCompatActivity {
             }
         });
 
+        deleteAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteAccount();
+            }
+        });
+
         logOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -64,7 +73,62 @@ public class settings_page extends AppCompatActivity {
         });
     }
 
+    private void accountData() {
+        databaseHelper = new DatabaseHelper(this);
+        Cursor result = databaseHelper.getData();
+        if (result.getCount()== 0) {
+            Toast.makeText(this, "Please create an account firstly", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        StringBuffer buffer = new StringBuffer();
+        while (result.moveToNext()){
+            buffer.append("Name :  " + result.getString(0) + "\n");
+            buffer.append("LastName :  " + result.getString(1) + "\n");
+            buffer.append("Email :  " + result.getString(2) + "\n");
+            buffer.append("Phone :  " + result.getString(3) + "\n"+ "\n"+ "\n");
+        }
+        AlertDialog.Builder builder = new AlertDialog.Builder(settings_page.this);
+        builder.setCancelable(true);
+        builder.setTitle("User Data");
+        builder.setMessage(buffer.toString());
+        builder.show();
+    }
 
+    private void deleteAccount(){
+        databaseHelper = new DatabaseHelper(this);
+        Cursor result = databaseHelper.getData();
+        if (result.getCount()== 0) {
+            Toast.makeText(this, "There isn't any account to delete", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        pref = getSharedPreferences("Data",MODE_PRIVATE);
+        AlertDialog.Builder builder = new AlertDialog.Builder(settings_page.this);
+        builder.setTitle("Are you sure?");
+        builder.setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                databaseHelper.deleteData();
+                Toast.makeText(getApplicationContext(), "Account Deleted", Toast.LENGTH_SHORT).show();
+
+                editor = pref.edit();
+                editor.clear();
+                editor.apply();
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
     private void logout() {
         pref = getSharedPreferences("Data",MODE_PRIVATE);
         editor = pref.edit();
@@ -75,26 +139,6 @@ public class settings_page extends AppCompatActivity {
     }
 
 
-    private void accountData() {
-        databaseHelper = new DatabaseHelper(this);
-        Cursor result = databaseHelper.getData();
-        if (result.getCount()== 0) {
-            Toast.makeText(this, "Please create an account firstly", Toast.LENGTH_SHORT).show();
-            return;
-        }
-            StringBuffer buffer = new StringBuffer();
-            while (result.moveToNext()){
-                buffer.append("Name :  " + result.getString(0) + "\n");
-                buffer.append("LastName :  " + result.getString(1) + "\n");
-                buffer.append("Email :  " + result.getString(2) + "\n");
-                buffer.append("Phone :  " + result.getString(3) + "\n"+ "\n"+ "\n");
-        }
-        AlertDialog.Builder builder = new AlertDialog.Builder(settings_page.this);
-        builder.setCancelable(true);
-        builder.setTitle("User Data");
-        builder.setMessage(buffer.toString());
-        builder.show();
-    }
 
 
 }
